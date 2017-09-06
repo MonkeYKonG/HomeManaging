41c1493bb034dd307bf0152e522ebe96aa97f2db
Alexis CRAYE <alexis1.craye@epitech.eu>
2017-09-06 13:03:33 +0200

    initialize angufire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,26 +9,37 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseUrl: "",
+    storageBucket: "",
+    messagingSenderId: ''
+};
+
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+	MyApp,
+	HomePage,
+	ListPage
+    ],
+    imports: [
+	BrowserModule,
+	IonicModule.forRoot(MyApp),
+	AngularFireModule.initializeApp(firebaseConfig)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+	MyApp,
+	HomePage,
+	ListPage
+    ],
+    providers: [
+	StatusBar,
+	SplashScreen,
+	{provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
 export class AppModule {}
